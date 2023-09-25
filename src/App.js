5524d06209bf09cfe230f0041fa8aaf411ffb844
Kimaru <ropdeno@gmail.com>
2023-09-25 05:25:17 +0300

    Adding content

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <h1> This is my first time using vim and git<h1>
       <p>Paragraph five added in local machine<p>
+      <p>Adding paragraph 6 from local<p>
     </div:
   );
 }
